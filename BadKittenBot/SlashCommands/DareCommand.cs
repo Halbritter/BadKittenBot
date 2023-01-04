97800a643fd354ce767159fcb87f2e48a2e770d8
Henrik Halbritter <henrik@halbritters.com>
2023-01-04 23:07:01 +0100

    Commit

---
+++ BadKittenBot/SlashCommands/DareCommand.cs
@@ -0,0 +1,38 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace BadKittenBot.SlashCommands;
+
+public class DareCommand : ISlashCommand
+{
+    public static readonly string DareID = "81432d5c-304c-4f2c-8805-ba622ef57473";
+
+    private List<string> dares = new List<string>()
+    {
+        "Haue deinen Pullermann auf den Tisch",
+        "Mach mit deinen Boobis Milch"
+    };
+
+    public string Name
+    {
+        get => "pflicht";
+    }
+
+    public async void Execute(SocketSlashCommand command)
+    {
+        var builder = new ComponentBuilder()
+            .WithButton("Wahrheit", TruthCommand.TruthID).WithButton("Pflicht", DareID);
+        command.RespondAsync(
+            dares[Random.Shared.Next(0, dares.Count - 1)],
+            components: builder.Build()
+        );
+    }
+
+    public SlashCommandProperties BuildCommand(DiscordSocketClient client)
+    {
+        return new SlashCommandBuilder()
+        {
+            Description = "Startet eine neue Runde Wahrheit oder Pflicht",
+        }.Build();
+    }
+}
---
+++ BadKittenBot/SlashCommands/TruthCommand.cs
@@ -0,0 +1,38 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace BadKittenBot.SlashCommands;
+
+public class TruthCommand : ISlashCommand
+{
+    public static readonly string TruthID = "c9ec6dcd-be91-4cf3-951d-f6f02e6240c4";
+
+    private List<string> truths = new List<string>()
+    {
+        "Bist du ein Opfer?",
+        "Wie lange ist dein Rüssel?"
+    };
+
+    public string Name
+    {
+        get => "wahrheit";
+    }
+
+    public async void Execute(SocketSlashCommand command)
+    {
+        var builder = new ComponentBuilder().WithButton("Wahrheit", TruthID).WithButton("Pflicht", DareCommand.DareID);
+
+        command.RespondAsync(
+            truths[Random.Shared.Next(0, truths.Count - 1)],
+            components: builder.Build()
+        );
+    }
+
+    public SlashCommandProperties BuildCommand(DiscordSocketClient client)
+    {
+        return new SlashCommandBuilder()
+        {
+            Description = "Startet eine neue Runde Wahrheit oder Pflicht",
+        }.Build();
+    }
+}
