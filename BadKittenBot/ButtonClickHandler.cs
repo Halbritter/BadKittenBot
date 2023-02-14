ff3e3d9529eeec4ce8e2e0dfd49eaad6b6ecdb6e
Henrik Halbritter <henrik.halbritter@lebenshilfe-buxtehude.de>
2023-02-14 09:22:33 +0100

    ButtonClickHandler in eigene datei verschoben

---
+++ BadKittenBot/ButtonClickHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using BadKittenBot.ButtonClicks;
+using Discord.WebSocket;
+
+namespace BadKittenBot;
+
+public class ButtonClickHandler
+{
+    private readonly DiscordSocketClient _client;
+    private readonly List<IButton>       _commands;
+    private readonly IEnumerable<Type>   _types;
+
+    public ButtonClickHandler(DiscordSocketClient client)
+    {
+        _client   = client;
+        _commands = new List<IButton>();
+        _types = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.Namespace == "BadKittenBot.ButtonClicks")
+            .Where(c => c.IsClass && c.IsAssignableTo(typeof(IButton)));
+        foreach (Type nestedType in _types)
+        {
+            IButton instance = Activator.CreateInstance(nestedType, args: _client) as IButton;
+            _commands.Add(instance);
+        }
+    }
+
+    public Task ButtonListener(SocketMessageComponent arg)
+    {
+        foreach (IButton command in _commands)
+        {
+            if (arg.Data.CustomId.StartsWith(command.Id))
+                command.Execute(arg);
+        }
+
+        return Task.CompletedTask;
+    }
+}
--- BadKittenBot/Program.cs
+++ BadKittenBot/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System.Configuration;
-using System.Reflection;
-using BadKittenBot.ButtonClicks;
 using Discord;
 using Discord.WebSocket;
 
@@ -51,36 +49,5 @@ public class Program
         await discordSocketClient.LoginAsync(TokenType.Bot, token);
         await discordSocketClient.StartAsync();
         return discordSocketClient;
-    }
-}
-
-public class ButtonClickHandler
-{
-    private readonly DiscordSocketClient _client;
-    private readonly List<IButton>       _commands;
-    private readonly IEnumerable<Type>   _types;
-
-    public ButtonClickHandler(DiscordSocketClient client)
-    {
-        _client   = client;
-        _commands = new List<IButton>();
-        _types = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.Namespace == "BadKittenBot.ButtonClicks")
-            .Where(c => c.IsClass && c.IsAssignableTo(typeof(IButton)));
-        foreach (Type nestedType in _types)
-        {
-            IButton instance = Activator.CreateInstance(nestedType, args: _client) as IButton;
-            _commands.Add(instance);
-        }
-    }
-
-    public Task ButtonListener(SocketMessageComponent arg)
-    {
-        foreach (IButton command in _commands)
-        {
-            if (arg.Data.CustomId.StartsWith(command.Id))
-                command.Execute(arg);
-        }
-
-        return Task.CompletedTask;
     }
 }
